995ee7d25d3abbef03982128140f1ad4911e2c09
ChefDK User <chef@ip-172-31-63-41.ec2.internal>
2017-03-02 17:24:13 +0000

    ohai node data in recipe

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,9 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello World</h1>'
+  content "<h1>Hello World</h1>
+           <h1>Hostname : #{node['hostname']}</h1>
+           <h1>IP Address : #{node['ipaddress']} </h1>"
 end
 
 service 'httpd' do
