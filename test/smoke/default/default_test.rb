699c36ea9bb10e9a46a17f4c722d0a84714fc590
ChefDK User <chef@ip-172-31-63-41.ec2.internal>
2017-03-02 16:25:33 +0000

    added inspec tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -13,6 +13,14 @@ unless os.windows?
 end
 
 describe port(80) do
-  it { should_not be_listening }
+  it { should be_listening }
   skip 'This is an example test, replace with your own test.'
+end
+
+describe file('/var/www/html/index.html') do
+  its('content') { should eq '<h1>Hello World</h1>' }
+end
+
+describe command('curl localhost') do
+   its('exit_status') { should eq 0 }
 end
